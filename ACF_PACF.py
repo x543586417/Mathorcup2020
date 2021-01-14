16e61e1f2beb7e71c4280c8fa8129229d833ac86
xrx1996 <543586417@qq.com>
2021-01-14 17:25:47 +0800

    对该代码进行了调整，更改列表可以选择基站

--- ACF_PACF.py
+++ ACF_PACF.py
@@ -2,11 +2,13 @@ from statsmodels.tsa.stattools import acf, pacf
 from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
 import matplotlib.pyplot as plt
 import pandas as pd
-#数据集
-df = pd.read_csv('https://raw.githubusercontent.com/selva86/datasets/master/a10.csv')
+
+# 数据集
+df = pd.read_csv('处理后数据/短期工作日排序.csv')
 # Calculate ACF and PACF upto 50 lags# acf_50 = acf(df.value, nlags=50)# pacf_50 = pacf(df.value, nlags=50)
 # Draw Plot
-fig, axes = plt.subplots(1, 2, figsize=(16, 3), dpi=100)
-plot_acf(df.value.tolist(), lags=50, ax=axes[0])
-plot_pacf(df.value.tolist(), lags=50, ax=axes[1])
-plt.show()
+for i in [186, 420, 5229, 9609]:
+    fig, axes = plt.subplots(2, 1, figsize=(8, 4), dpi=100)
+    plot_acf(df[df['Cell_num'] == i]['Download'].tolist(), lags=50, ax=axes[0])
+    plot_pacf(df[df['Cell_num'] == i]['Download'].tolist(), lags=50, ax=axes[1])
+    plt.show()
